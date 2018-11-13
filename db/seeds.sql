a37304a274a13816dcb940a7d63352c715bbcbfd
CircuitSyn <circuitsyn@gmail.com>
2018-11-13 15:42:45 -0800

    mysql seeds added

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE shake_db;
+
+INSERT INTO shakes (Shake) VALUES ('Double Brownie Vanilla Swirl with Rum');
+INSERT INTO shakes (Shake) VALUES ('Peanut Butter Jelly and Cognac Time');
+INSERT INTO shakes (Shake, Drank) VALUES ('Smortastic', true);
+INSERT INTO shakes (Shake, Drank) VALUES ('Cupcake Monsoon', true);
+INSERT INTO shakes (Shake, Drank) VALUES ('Coco-tastic Non-Dairy', true);
+INSERT INTO shakes (Shake) VALUES ('Moose Tracks A La Mode');
